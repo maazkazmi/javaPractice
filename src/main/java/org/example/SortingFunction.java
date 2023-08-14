4931c221cb8e51fe0876fd9fe455512c450b1371
Muhammad Umair Khan <49487735+UmairKhan13@users.noreply.github.com>
2023-08-15 00:31:30 +0500

    sorting code

--- src/main/java/org/example/SortingFunction.java
+++ src/main/java/org/example/SortingFunction.java
@@ -1,20 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class SortingFunction
 {
-    public static void sortingFunc(int[] array)
-    {
-        // Write sorting code
-    }
+
     public static void main(String[] args)
     {
-
+        Scanner scanner = new Scanner(System.in);
         int[] a = new int[5]; // assign memory
 
         // TAKE INPUT FROM USER USING LOOP
+        for (int i = 0; i < a.length; i++)
+        {
+            a[i] = scanner.nextInt();
+        }
 
         // sort the array using func.
-        sortingFunc(a);
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = 0; j < a.length; j++)
+            {
+                int temp = 0;
+
+                if(a[j] < a[i])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nSorted Array:");
+
+        for (int b : a)
+        {
+            System.out.println(b);
+        }
+
+
+
+
+
     }
 
 
