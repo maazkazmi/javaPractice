77e2819e4a1a9be8f2cdfc0b277434102869c107
Muhammad Umair Khan <49487735+UmairKhan13@users.noreply.github.com>
2023-08-15 00:12:11 +0500

    sort

---
+++ src/main/java/org/example/SortingFunction.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class SortingFunction
+{
+    public static void sortingFunc(int[] array)
+    {
+        // Write sorting code
+    }
+    public static void main(String[] args)
+    {
+
+        int[] a = new int[5]; // assign memory
+
+        // TAKE INPUT FROM USER USING LOOP
+
+        // sort the array using func.
+        sortingFunc(a);
+    }
+
+
+}
