68917fa1977b6f151a1f716cbd26417f3aa6c5bb
Maaz Kazmi <khizrkazmi@Khizrs-Air.attlocal.net>
2023-08-14 14:00:08 -0400

    solved  number parity question

--- src/main/java/org/example/NumberParity.java
+++ src/main/java/org/example/NumberParity.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class NumberParity {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
 
-    public static void main(String[] args)
-    {
+        System.out.print("Enter an integer: ");
+        int number = myObj.nextInt();
 
+        if (number % 2 == 0) {
+            System.out.println("your  num is even");
+        } else {
+            System.out.println("your num is odd");
+        }
+
+        myObj.close();
     }
-
 }
---
+++ src/main/java/org/example/calculator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -,*,/): ");
+
+        double result;
+
+
+    }}
