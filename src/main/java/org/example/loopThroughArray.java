a2d3edf4554b7b92b8ba6b40c8e8c6136c15fd25
Muhammad Umair Khan <49487735+UmairKhan13@users.noreply.github.com>
2023-08-15 22:43:18 +0500

    looping through array

---
+++ src/main/java/org/example/loopThroughArray.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class loopThroughArray
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+
+        String[] cars = new String[5];
+
+        for(int i = 0; i < cars.length ; i ++) // take input in loop
+        {
+            cars[i] = obj.nextLine();
+        }
+
+        for (String car : cars) // print loop
+        {
+            System.out.println(car);
+        }
+
+    }
+}
