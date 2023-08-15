69548640c5eefedd295d9411cf2ed3efbba35857
Muhammad Umair Khan <49487735+UmairKhan13@users.noreply.github.com>
2023-08-15 22:36:53 +0500

    reverse int code

--- src/main/java/org/example/problem3.java
+++ src/main/java/org/example/problem3.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 // Write a program that prompts the user to input an
 // integer and then outputs the number with the digits reversed.
 // For example, if the input is 12345, the output should be 54321.
-public class problem3 {
-    public static void main(String[] args){
+public class problem3
+{
+    public static void main(String[] args)
+    {
 
-    Scanner myObj = new Scanner(System.in);
-    int number;
+      Scanner myObj = new Scanner(System.in);
+      int number;
 
-    System.out.println("enter number: ");
-    number = myObj.nextInt();
+      System.out.println("enter number: ");
+      number = myObj.nextInt(); // 2785
 
+      int reverse = 0; // 0
 
+      while(number != 0)
+      {
+          int digit = number % 10; // 5 8 7 2
+          reverse = reverse * 10  + digit; // 5 58 587 5872
+          number /= 10; // 278 27 2 0
+      }
 
+      System.out.print(reverse);
 
-
-
-
-
-}}
+    }
+}
