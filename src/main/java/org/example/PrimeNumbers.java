812d160f6fbb9938a4cb185f57ad8fb5086d10ff
Maaz Kazmi <khizrkazmi@Khizrs-Air.attlocal.net>
2023-08-20 15:23:19 -0400

    PrimeNumbers

---
+++ src/main/java/org/example/PrimeNumbers.java
@@ -0,0 +1,23 @@
+package org.example;
+//Prime Number Detection: Develop a program to determine whether a given number is prime or not.
+//2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter a int");
+        int num  = myObj.nextInt();
+
+        if(Prime(num)){
+            System.out.println(num  +  );
+        }
+    }
+
+
+
+
+}
