d0ec63bfb5c1c62a14fd7bc983ea92a95572852c
Muhammad Umair Khan <49487735+UmairKhan13@users.noreply.github.com>
2023-08-21 22:22:06 +0500

    Linear Search

--- src/main/java/org/example/LinearSearch.java
+++ src/main/java/org/example/LinearSearch.java
@@ -3,33 +3,49 @@ package org.example;
 import java.util.Scanner;
 
 public class LinearSearch {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Enter the number of elements: ");
-//        int n = scanner.nextInt();
-//
-//        int[] arr = new int[n];
-//
-//        System.out.println("Enter the elements :");
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = scanner.nextInt();
-//        }
-//
-//        System.out.print("Enter the element to search for: ");
-//        int target = scanner.nextInt();
-//
-//        int index = linearSearch (arr, target);
-//
-//        if (index != -1) {
-//            System.out.println("Element found at index " + index);
-//        } else {
-//            System.out.println("Element not found in the array.");
-//        }
-//
-//        scanner.close();
-//    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the element to search for: ");
+        int target = scanner.nextInt();
+
+        int index = linearSearch(arr, target);
+
+        if (index != -1)
+        {
+            System.out.println("Element found at index " + index);
+        }
+        else
+        {
+            System.out.println("Element not found in the array.");
+        }
+
+        scanner.close();
     }
+
+    private static int linearSearch(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+}
 
