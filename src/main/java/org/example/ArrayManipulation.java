4d6f1a531d80c893899f0b4d6628bb2ff7d532f3
Muhammad Umair Khan <49487735+UmairKhan13@users.noreply.github.com>
2023-08-21 22:51:33 +0500

    Array manipulation

--- src/main/java/org/example/ArrayManipulation.java
+++ src/main/java/org/example/ArrayManipulation.java
@@ -1,9 +1,43 @@
 package org.example;
 
-public class ArrayManipulation
-{
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
 
+public class ArrayManipulation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE; // Initialize max with the smallest possible integer value
+        int min = Integer.MAX_VALUE; // Initialize min with the largest possible integer value
+
+        for (int num : numbers) {
+            sum += num;
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
     }
 }
